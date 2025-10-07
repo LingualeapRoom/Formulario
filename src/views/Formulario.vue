179383af71fa0ccbd2470e4d448360618c38bae2
Andres Navas <andresconete@gmail.com>
2025-10-06 18:28:14 -0600

    El primer commit

---
+++ src/views/Formulario.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h2>Formulario de Contacto</h2>
+        <form>
+            <div>
+                <label for="nombre">Nombre:</label><br>
+                <input type="text" id="nombre" name="nombre">
+            </div>
+            <br>
+            
+            <div>
+                <label for="email">Email:</label><br>
+                <input type="email" id="email" name="email">
+            </div>
+            <br>
+            
+            <div>
+                <label for="mensaje">Mensaje:</label><br>
+                <textarea id="mensaje" name="mensaje" rows="4"></textarea>
+            </div>
+            <br>
+            
+            <button type="submit">Enviar</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default { 
+        name: 'Formulario', 
+    } 
+</script>
+
+<style>
+    
+</style>
